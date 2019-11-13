c800901501b29986d14f74b05cf2e45a552502ac
Daniel Chincoya <chincoya@ciencias.unam.mx>
2019-11-13 17:55:11 -0600

    Finish categories pages

--- scripts/product.js
+++ scripts/product.js
@@ -1,6 +1,5 @@
 var menuHide = true;
 var unset = "";
-let cards = document.getElementsByClassName("category-prod-card")
 
 function menuOpener() {
   if(menuHide){
@@ -12,10 +11,6 @@ function menuOpener() {
     menuHide = true;
     document.getElementById("menu-button-icon").className = "fas fa-grip-lines";
   }
-}
-console.log(cards);
-for(let card of cards) {
-  card.style = "background-image: url(https://raw.githubusercontent.com/Chincoya/temp-test/master/"+(document.title.toLowerCase())+"/"+card.id+"/1.jpg);";
 }
 
 document.getElementById("menu-button").addEventListener("click", menuOpener);
